6db6c93283aeca50140c60edb306c8229c280250
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-07-26 22:58:15 -0300

    Update ex04.py

--- 1_comandos_condicionais/ex04.py
+++ 1_comandos_condicionais/ex04.py
@@ -3,9 +3,9 @@ Escreva um programa que, dados três números inteiros, imprima os números em
 ordem crescente.
 '''
 
-num_1 = int(input())
-num_2 = int(input())
-num_3 = int(input())
+num_1 = int(input()) #menor número
+num_2 = int(input()) #número do meio
+num_3 = int(input()) #maior número
 
 if num_1 > num_2:
     num_2, num_1 = num_1, num_2
